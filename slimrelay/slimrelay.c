2f2ee8fd164861a94c0a2e9e2b3d466ee27de79b
ian <Ian Good icgood@gmail.com>
2010-10-04 12:49:59 -0400

    Nothing to say

--- slimrelay/slimrelay.c
+++ slimrelay/slimrelay.c
@@ -1,9 +1,40 @@
-#include "slimrelay.h"
+#include <stdio.h>
+#include <string.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#include <luah.h>
+
+/* {{{ push_argvs_to_global() */
+static void push_argvs_to_global (lua_State *L, int argc, char **argv)
+{
+	int i;
+	lua_newtable (L);
+	for (i=0; i<argc; i++)
+	{
+		lua_pushstring (L, argv[i]);
+		lua_rawseti (L, -2, i);
+	}
+	lua_setglobal (L, "arg");
+}
+/* }}} */
 
 /* {{{ main() */
 int main (int argc, char *argv[])
 {
-	printf ("Hello, World!\n");
+	int ret;
+
+	lua_State *L = luaL_newstate ();
+	luaL_openlibs (L);
+
+	push_argvs_to_global (L, argc, argv);
+	if (luaL_dofile (L, "master.lua") != 0)
+		return lua_error (L);
+
+	lua_close (L);
+
 	return 0;
 }
 /* }}} */
--- slimrelay/slimrelay.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SLIMTA_SLIMRELAY_H
-#define __SLIMTA_SLIMRELAY_H
-
-
-
-#endif
-// vim:foldmethod=marker:ai:ts=4:sw=4:
