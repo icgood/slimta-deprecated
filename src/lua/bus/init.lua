5f4b882cf960e614ea16a2ea990c48b349814d34
Ian Good <ian.good@rackspace.com>
2012-02-06 15:13:11 +0000

    chain() convenience function

--- src/lua/bus/init.lua
+++ src/lua/bus/init.lua
@@ -1,5 +1,6 @@
 
 require "slimta"
+require "ratchet"
 require "ratchet.bus.samestate"
 
 slimta.bus = {}
@@ -22,6 +23,31 @@ end
 -- {{{ slimta.bus.new_client()
 function slimta.bus.new_client(...)
     return slimta.bus.client.new(...)
+end
+-- }}}
+
+-- {{{ slimta.bus.chain()
+function slimta.bus.chain(calls, from_bus)
+    local call_stacks = {}
+    local call_from_bus, call_to_bus, prev_from_bus
+
+    for i, call in ipairs(calls) do
+        call_from_bus, call_to_bus = slimta.bus.new_local()
+        call_stacks[i] = {call, prev_from_bus, call_to_bus}
+        prev_from_bus = call_from_bus
+    end
+
+    if call_stacks[1] then
+        call_stacks[1][2] = from_bus
+    end
+
+    local final_to_bus
+    for i, stack in ipairs(call_stacks) do
+        ratchet.thread.attach(table.unpack(stack))
+        final_to_bus = stack[3]
+    end
+
+    return final_to_bus
 end
 -- }}}
 
